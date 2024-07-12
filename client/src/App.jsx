4298cfc959ead08cf13e1664ae56365e9d5c1e7f
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-07-12 13:36:26 +0530

    create header component & add Flowbite react Ui libary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,11 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Dashbord from "./pages/Dashbord"
 import Projects from "./pages/Projects"
-
+import Header from './components/Header'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
