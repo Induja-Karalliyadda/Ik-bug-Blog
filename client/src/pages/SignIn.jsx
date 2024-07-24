1f6e00047b36e000fcbe3d7751c5eaa2e1399dcb
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-07-24 14:23:21 +0530

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,104 @@
 
-function SignIn() {
-  return (
-    <div></div>
-  );
-}
+import { Link,useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
 
-export default SignIn;
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const[errorMessage,setErrorMessages]= useState(null);
+  const[loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange =(e) =>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()})
+  };
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Correct method name
+    if( !formData.email ||!formData.password){
+      return setErrorMessages('Please fill out all fields')
+    }
+    try {
+      setLoading(true);
+      setErrorMessages(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+     
+      const data = await res.json();
+      // Handle the response data
+      console.log(data);
+      if(data.success === false){
+        return setErrorMessages(data.message)
+      }
+      setLoading(false)
+      if(res.ok){
+        navigate('/')
+      }
+    } catch (error) {
+     setErrorMessages(error.message)
+     setLoading(false)
+    }
+  };
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className=" font-bold dark:text-white text-4xl">
+            <samp className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Ik bug
+            </samp>
+            Blog
+          </Link>
+          <p className="text-sm mt-5"> 
+            Reading makes man complete </p>
+            <p>You can sign In with your email and password or with google   </p>
+            
+        </div>
+        {/* right */}
+        <div className="flex-1">
+        <div className="flex flex-col gap-4 " onSubmit={handleSubmit}>
+          <form >
+            
+            <div>
+              <Label value="Your email"/>
+              <TextInput type="emai" placeholder="name@company.com" id="email" onChange={handleChange} />
+            </div>
+            <div>
+              <Label value="Your password"/>
+              <TextInput type="password" placeholder="Password" id="password" onChange={handleChange} />
+            </div>
+            <br />
+            <Button  gradientDuoTone='purpleToPink' type="submit" disabled={loading} > 
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm'/>
+                  <span className="pl-3">Loading...</span>
+                  </>
+                  
+                ):'Sign In'
+              }
+
+            </Button>
+          </form>
+              <div className="flex gap-2 text-sm mt-5">
+                <span>Dont Have an account?</span>
+                <Link to='/sign-up' className="text-blue-500">Sign Up</Link>
+              </div>
+      </div>
+      {
+        errorMessage && (
+          <Alert className="mt-5" color='failure'>
+            {errorMessage}
+          </Alert>
+        )
+      }
+      </div>
+      </div>
+      </div>
+   
+  )
+}
