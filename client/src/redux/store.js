6bfefec272203015e3ffc4220633e2492fb3e7c9
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-08-05 11:03:04 +0530

    complete dark mode functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
-import { persistReducer as reduxPersistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import themeReducer from './Theme/themeSlice'; // Corrected import
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/es/persistStore';
 
 const rootReducer = combineReducers({
   user: userReducer,
-})
+  theme: themeReducer,
+});
 
 const persistConfig = {
   key: 'root',
@@ -14,11 +16,14 @@ const persistConfig = {
   version: 1,
 };
 
-const persistedReducer = reduxPersistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
