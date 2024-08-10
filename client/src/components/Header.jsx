3bd2aa0da2cfe8b53a33c1c7d1b5c800ae9c8f26
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-08-10 22:30:14 +0530

    complete sidebar of the dashbord

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon , FaSun} from "react-icons/fa";
-import { useSelector, useDispatch } from 'react-redux'; 
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from 'react-redux'; // Keep only this import
 import { toggleTheme } from "../redux/Theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
+  
   const dispatch = useDispatch();
   const { currentUser } = useSelector(state => state.user);
   const {theme } = useSelector ((state)=>state.theme)
@@ -32,7 +33,7 @@ export default function Header() {
       </Button>
       <div className="flex gap-2 md:order-2">
         <button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={() => dispatch(toggleTheme())}>
-         {theme === 'light'? <FaSun/>:<FaMoon/>}
+          {theme === 'light'? <FaSun/>:<FaMoon/>}
          
         </button>
         {currentUser ? (
